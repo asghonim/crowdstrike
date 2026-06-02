5c235d7429fe9794c9249486ef5a7fc8bc10e2b6
Ghonim <asghonim@live.com>
2026-06-02 12:49:33 +0000

    Init

---
+++ tests/ClientTest.php
@@ -0,0 +1,15 @@
+<?php
+
+use Asghonim\Crowdstrike\Client;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\TestCase;
+
+#[CoversClass(Client::class)]
+class ClientTest extends TestCase
+{
+  public function testItWorks(): void
+  {
+    $c = new Client();
+    $this->assertEquals(1, 1);
+  }
+}
